26f6543ceabcbebd805f48af91249b540944eeb3
Jade Webb <66337093+jadewebb@users.noreply.github.com>
2021-11-21 21:15:03 -0800

    Minor updates to repos

--- server/src/main/java/net/tbp/interval/database/GoalRepository.java
+++ server/src/main/java/net/tbp/interval/database/GoalRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import net.tbp.interval.backup.Event;
+import net.tbp.interval.backup.Goal;
 
 
 /**
